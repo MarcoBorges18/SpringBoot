c99c9f4daceee338a60bd26155551a41cfb65da8
marco <antonioborges.marco@gmail.com>
2023-08-14 15:26:54 -0300

    Implementado o cadastro de medicos com suas respectivas validações

--- src/main/java/com/marco/api/controller/MedicoController.java
+++ src/main/java/com/marco/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package com.marco.api.controller;
 
 import com.marco.api.medico.DadosCadastroMedico;
+import com.marco.api.medico.Medico;
+import com.marco.api.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medico")
 public class MedicoController{
+
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public  void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
     }
 }
