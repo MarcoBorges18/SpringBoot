bc8983c8fb1c40af45b4f0f785f0b1e6c5a21cde
marco <antonioborges.marco@gmail.com>
2023-08-14 11:13:28 -0300

    Adicionando metodo get do HelloController e metodo post do MedicoControlelr

--- src/main/java/com/marco/api/controller/MedicoController.java
+++ src/main/java/com/marco/api/controller/MedicoController.java
@@ -1,14 +1,15 @@
 package com.marco.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/medico")
-public class MedicoController {
+public class MedicoController{
     @PostMapping
-    public  void cadastrar(){
-
+    public  void cadastrar(@RequestBody String json){
+        System.out.println(json);
     }
 }
