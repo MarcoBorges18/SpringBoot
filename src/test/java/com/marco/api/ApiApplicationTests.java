ca0160089d63892bcddf1dc4e1ca262dd2f2106d
marco <antonioborges.marco@gmail.com>
2023-08-14 09:53:27 -0300

    first commit

---
+++ src/test/java/com/marco/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
